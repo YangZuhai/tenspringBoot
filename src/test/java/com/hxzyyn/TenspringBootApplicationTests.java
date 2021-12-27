b4f8110e998537f37af89f163d843b3b6578d560
YangZuhai <2840723492@qq.com>
2021-12-27 15:03:11 +0800

    提交...

---
+++ src/test/java/com/hxzyyn/TenspringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxzyyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenspringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
